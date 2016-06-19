bea74bb1d414ec69ac9adc7f6e66ece04457dca1
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-06-19 21:46:17 +0200

    Start Editor parts

---
+++ app/src/main/java/fr/supinternet/codewritter3k/EditorActivity.java
@@ -0,0 +1,26 @@
+package fr.supinternet.codewritter3k;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.EditText;
+
+import butterknife.BindView;
+
+/**
+ * Created by AwH on 19/06/16.
+ */
+public class EditorActivity extends AppCompatActivity {
+
+    public String language;
+    @BindView(R.id.editText)
+    EditText userCode;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.language = getIntent().getExtras().getString("language");
+        Log.d("LANGUAGE", this.language);
+    }
+}
--- app/src/main/java/fr/supinternet/codewritter3k/MainActivity.java
+++ app/src/main/java/fr/supinternet/codewritter3k/MainActivity.java
@@ -1,9 +1,11 @@
 package fr.supinternet.codewritter3k;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.main_activity_html_css_btn) public void goToHTMLAndCSS() {
+        Intent editorIntent = new Intent(this, EditorActivity.class);
+        editorIntent.putExtra("language", "HTML/CSS");
+        startActivity(editorIntent);
     }
 }
